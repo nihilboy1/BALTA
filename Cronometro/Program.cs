4dd26481bfbaaa16de2cfb4b1ea1e6c636336043
nihilboy1 <samuelseve1@gmail.com>
2023-05-29 17:36:01 -0300

    Finalização do projeto cronometro

---
+++ Cronometro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program{
+
+        static void Main(string[] args){
+            int waitTime = Menu();
+            Start(waitTime);
+        }
+
+
+        static int Menu(){
+            Console.WriteLine("Informe o tempo que deseja esperar (Em segundos): ");
+            int waitTime =  int.Parse(Console.ReadLine());
+            return waitTime;
+        }
+
+
+
+        static void Start(int time){
+         
+            int currentTime = time;
+            while(currentTime >= 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Aguardando {time} segundos");
+                Console.WriteLine(currentTime);
+                currentTime--;
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Thread.Sleep(1500);
+            Console.WriteLine("Contagem finalizada.");
+            int waitTime = Menu();
+            Start(waitTime);
+        }
+    }
+}
